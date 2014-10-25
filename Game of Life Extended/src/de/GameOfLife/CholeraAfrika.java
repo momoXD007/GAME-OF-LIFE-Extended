1a0cf16771748d54e2e253e9afaf8d575944bdb8
Inga <Inga.Miadowicz@gmx.de>
2014-10-25 15:55:31 +0200

    Korrektur einiger Klassen vergessen

--- Game of Life Extended/src/de/GameOfLife/CholeraAfrika.java
+++ Game of Life Extended/src/de/GameOfLife/CholeraAfrika.java
@@ -3,84 +3,17 @@ package de.GameOfLife;
 public class CholeraAfrika extends Krankheiten {
 	
 	//Choleraepedemie (Simbabwe 2008/2009)
-	CholeraAfrika virusCholera = new CholeraAfrika();
-
+	
 	public CholeraAfrika() {
-		super();
-		virusCholera.setTierAufTier(false);
-		virusCholera.setMenschAufTier(false);
-		virusCholera.setTierAufMensch(false);	
-		virusCholera.setMenschAufMensch(true);
+		this.setTierAufTier(false);
+		this.setMenschAufTier(false);
+		this.setTierAufMensch(false);	
+		this.setMenschAufMensch(true);
 		
-		virusCholera.setInfektionsRate(0.0077);;
-		virusCholera.setHeilungsRate(0.4925);
-		virusCholera.setSterbeRate(0.053);	
-		virusCholera.setResistenzRate(0);
+		this.setInfektionsRate(0.0077);;
+		this.setHeilungsRate(0.4925);
+		this.setSterbeRate(0.053);	
+		this.setResistenzRate(0);
 	}
 	
-	public void setTierAufMensch(boolean TaM){
-		tierAufMensch = TaM;
-	}
-	
-	public boolean getTierAufMensch(){
-		return tierAufMensch;
-	}
-	
-	public void setTierAufTier(boolean auss){
-		tierAufTier = auss;
-	}
-	
-	public boolean getTierAufTier(){
-		return tierAufTier;
-	}
-	
-	public void setMenschAufMensch(boolean auss){
-		menschAufMensch = auss;
-	}
-	
-	public boolean getMenschAufMensch(){
-		return menschAufMensch;
-	}
-	
-	public void setMenschAufTier(boolean auss){
-		menschAufTier = auss;
-	}
-	
-	public boolean getMenschAufTier(){
-		return menschAufTier;
-	}
-	
-	public void setInfektionsRate(double auss){
-		infektionsRate = auss;
-	}
-	
-	public double getInfektionsRate(){
-		return infektionsRate;
-	}
-
-	public void setHeilungsRate(double auss){
-		heilungsRate = auss;
-	}
-	
-	public double getHeilungsRate(){
-		return heilungsRate;
-	}	
-	
-	public void setSterbeRate(double auss){
-		sterbeRate = auss;
-	}
-	
-	public double getSterbeRate(){
-		return sterbeRate;
-	}
-	
-	public void setResistenzRate(double auss){
-		resistenzRate = auss;
-	}
-	
-	public double getResistenzRate(){
-		return resistenzRate;
-	}
-
-
 }
--- Game of Life Extended/src/de/GameOfLife/Tollwut.java
+++ Game of Life Extended/src/de/GameOfLife/Tollwut.java
@@ -3,84 +3,17 @@ package de.GameOfLife;
 public class Tollwut extends Krankheiten{
 
 	//Tollwut heute weltweit
-	Tollwut virusTollwut = new Tollwut();
 
 	public Tollwut() {
-		super();
-		virusTollwut.setTierAufTier(true);
-		virusTollwut.setMenschAufTier(false); //kommt auch nicht vor(!)
-		virusTollwut.setTierAufMensch(true);	
-		virusTollwut.setMenschAufMensch(false); //kommt nicht vor(!)
+		this.setTierAufTier(true);
+		this.setMenschAufTier(false); //kommt auch nicht vor(!)
+		this.setTierAufMensch(true);	
+		this.setMenschAufMensch(false); //kommt nicht vor(!)
 		
-		virusTollwut.setInfektionsRate(0.0014);;
-		virusTollwut.setHeilungsRate(0);
-		virusTollwut.setSterbeRate(0.7857 * (Math.pow(10, -5)));	
-		virusTollwut.setResistenzRate(0);
+		this.setInfektionsRate(0.0014);;
+		this.setHeilungsRate(0);
+		this.setSterbeRate(0.7857 * (Math.pow(10, -5)));	
+		this.setResistenzRate(0);
 	}
-	
-	public void setTierAufMensch(boolean TaM){
-		tierAufMensch = TaM;
-	}
-	
-	public boolean getTierAufMensch(){
-		return tierAufMensch;
-	}
-	
-	public void setTierAufTier(boolean auss){
-		tierAufTier = auss;
-	}
-	
-	public boolean getTierAufTier(){
-		return tierAufTier;
-	}
-	
-	public void setMenschAufMensch(boolean auss){
-		menschAufMensch = auss;
-	}
-	
-	public boolean getMenschAufMensch(){
-		return menschAufMensch;
-	}
-	
-	public void setMenschAufTier(boolean auss){
-		menschAufTier = auss;
-	}
-	
-	public boolean getMenschAufTier(){
-		return menschAufTier;
-	}
-	
-	public void setInfektionsRate(double auss){
-		infektionsRate = auss;
-	}
-	
-	public double getInfektionsRate(){
-		return infektionsRate;
-	}
-
-	public void setHeilungsRate(double auss){
-		heilungsRate = auss;
-	}
-	
-	public double getHeilungsRate(){
-		return heilungsRate;
-	}	
-	
-	public void setSterbeRate(double auss){
-		sterbeRate = auss;
-	}
-	
-	public double getSterbeRate(){
-		return sterbeRate;
-	}
-	
-	public void setResistenzRate(double auss){
-		resistenzRate = auss;
-	}
-	
-	public double getResistenzRate(){
-		return resistenzRate;
-	}
-
 	
 }
